4ef0c44adeb2697b3e53fc0b51e9e051f93df638
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:59 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostProcessConan(ConanFile):
     name = "boost_process"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_process"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["process"]
     is_header_only = True
 
